80b9ef283b9386400ba89690b0d2b9b839d84594
Cristian Franco <cristianfranco@Cristians-Mac-mini.local>
2022-11-30 22:36:26 -0800

    Day 1, puzzle 1 and 2 completed

---
+++ Day1/Puzzle1/main.py
@@ -0,0 +1,31 @@
+
+
+def process_input():
+    elf_list = []
+
+    file = open('input.txt', 'r')
+
+    single_elf_sum = 0
+    for line in file:
+        if(line != '\n'):
+            single_elf_sum += int(line)
+        else:
+            elf_list.append(single_elf_sum)
+            single_elf_sum = 0
+
+    return elf_list
+
+
+def main():
+    # process input file
+    elf_list = process_input()
+
+    # sort list in descending order
+    elf_list.sort(reverse=True)
+    print(elf_list[0])
+    
+
+
+
+if __name__ == "__main__":
+    main()
